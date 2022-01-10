bf7cf4874728df0b50b8a4cc3ea92b74e4150681
surajp <suraj.p@factset.com>
2022-01-10 15:07:55 +0530

    Fix: fixing gitignore

---
+++ notes-app/utils.js
@@ -0,0 +1,11 @@
+/** The following file is a dummy file. This is not required. */
+console.log('Running utils.js...')
+
+const name = 'Rambo';
+
+const add = function (a, b) {
+    return a + b;
+}
+
+// module.exports = name;
+module.exports = add;
