2e2be604fba011ef2c9fa65bdd303e66dfcf8c8c
surajp <suraj.p@factset.com>
2022-01-10 15:04:25 +0530

    Cleanup: Add files to gitignore to prevent tracking.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,15 @@
-const fs = require('fs');
-fs.writeFileSync('notes.txt', 'This file was created by node.js\n');
-fs.appendFileSync('notes.txt', 'This line was however appended by node.js');
+/**
+ *  const fs = require('fs');
+ *  fs.writeFileSync('notes.txt', 'This file was created by node.js\n');
+ *  fs.appendFileSync('notes.txt', 'This line was however appended by node.js');
+
+ *  const utils = require('./utils.js')
+ *  const add = require('./utils.js')
+ *  console.log(utils);
+ *  const sum = add(2, 3);
+ *  console.log(sum);
+ */
+
+const getNotes = require('./notes.js')
+
+console.log(getNotes());
