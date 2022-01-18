02694c5bf1745f909a4a3c2cb6f0d7d202fcf77b
Suraj P <surajperumallapalli97@gmail.com>
2022-01-18 16:15:54 +0530

    New: Added text formatting. Update to fetch temperature/feelslike temperature from http call.

--- weather-app/app.js
+++ weather-app/app.js
@@ -11,9 +11,16 @@
 // npm modules
 const postmanRequest = require('postman-request');
 
+// custom modules
+const textFormat = require('./text-formatter');
+
 const url = "http://api.weatherstack.com/current?access_key=688b742c07634c35077e590aefaa25ad&query=Chennai"
 
 postmanRequest({url: url, json: true}, (error, response) => {
-    // const data = JSON.parse(response.body);
-    console.log(response.body.current);
+    console.log('It is currently '
+        + textFormat.responseParameters(response.body.current.temperature)
+        + ' degrees outside.');
+    console.log('However, it feels like '
+        + textFormat.responseParameters(response.body.current.feelslike)
+        + ' degrees outside.');
 });
---
+++ weather-app/text-formatter.js
@@ -0,0 +1,8 @@
+// module that defines/exports types of text formats
+const chalk = require('chalk')
+
+const responseParameters = chalk.bold.red;
+
+module.exports = {
+    responseParameters: responseParameters
+};
